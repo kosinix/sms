671d995b1a4239d418f577441b7d55b618a6b3ba
kosinix <kosinix@users.noreply.github.com>
2022-01-11 23:53:42 +0800

    Remove files for demo

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const serialportgsm = require('serialport-gsm');
 (async () => {
     try {
 
-
         let modem = serialportgsm.Modem()
-
         let options = {
             baudRate: 9600,
             dataBits: 8,
@@ -17,7 +15,7 @@ const serialportgsm = require('serialport-gsm');
             xon: false,
             xoff: false,
             xany: false,
-            autoDeleteOnReceive: false,
+            autoDeleteOnReceive: true,
             enableConcatenation: true,
             incomingCallIndication: true,
             incomingSMSIndication: true,
@@ -25,47 +23,6 @@ const serialportgsm = require('serialport-gsm');
             customInitCommand: '',
             // logger: console
         }
-
-
-
-        modem.on('open', async result => {
-            console.log(`${result.request} ${result.data.modem} ${result.status}`)
-
-            result = await modem.initializeModem()
-            console.log(`${result.data}`)
-
-            result = await modem.getSimInbox()
-            console.log('getSimInbox', result.data)
-
-            result = await modem.deleteAllSimMessages()
-            console.log('deleteAllSimMessages', result.data)
-
-            result = await modem.getModemSerial()
-            console.log('modem serial', result.data.modemSerial)
-
-            result = await modem.getNetworkSignal()
-            console.log('signal', result.data)
-
-            result = await modem.getOwnNumber()
-            console.log('getOwnNumber', result.data)
-
-        })
-
-        modem.on('onSendingMessage', async result => {
-            console.log(result)
-        })
-
-        modem.on('onNewMessage', async result => {
-            console.log('onNewMessage', result)
-        })
-
-        modem.on('onNewMessageIndicator', async result => {
-            console.log('onNewMessageIndicator', result)
-        })
-
-        await modem.open('COM10', options)
-
-
 
     } catch (err) {
         console.log(err)
